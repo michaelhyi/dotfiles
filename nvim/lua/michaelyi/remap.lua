df1554ee9ecf0994ee54fe90330c0068a4312600
michaelhyi <michaelyi2004@gmail.com>
2024-04-25 00:14:14 -0400

    feat(diagnostic): setup remap to view diagnostic

--- nvim/lua/michaelyi/remap.lua
+++ nvim/lua/michaelyi/remap.lua
@@ -16,3 +16,32 @@ map('n', '<leader>dc', '<Cmd>DiffviewClose<CR>', opts)
 
 map('n', '<leader>js', '<Cmd>JdtWipeDataAndRestart<CR>', opts)
 
+
+vim.diagnostic.config({
+  virtual_text = false, -- Turn off inline diagnostics
+})
+
+-- Use this if you want it to automatically show all diagnostics on the
+-- current line in a floating window. Personally, I find this a bit
+-- distracting and prefer to manually trigger it (see below). The
+-- CursorHold event happens when after `updatetime` milliseconds. The
+-- default is 4000 which is much too long
+
+-- Show all diagnostics on current line in floating window
+vim.api.nvim_set_keymap(
+  'n', '<leader>d', ':lua vim.diagnostic.open_float()<CR>', 
+  { noremap = true, silent = true }
+)
+-- Go to next diagnostic (if there are multiple on the same line, only shows
+-- one at a time in the floating window)
+vim.api.nvim_set_keymap(
+  'n', '<leader>n', ':lua vim.diagnostic.goto_next()<CR>',
+  { noremap = true, silent = true }
+)
+-- Go to prev diagnostic (if there are multiple on the same line, only shows
+-- one at a time in the floating window)
+vim.api.nvim_set_keymap(
+  'n', '<leader>p', ':lua vim.diagnostic.goto_prev()<CR>',
+  { noremap = true, silent = true }
+)
+
