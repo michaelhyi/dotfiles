da777e417cafebb81306618d67d6f87b6319c309
michaelhyi <michaelyi2004@gmail.com>
2024-06-02 13:56:22 -0500

    feat(nvim): vim plugin update

--- nvim/lua/michaelyi/lazy.lua
+++ nvim/lua/michaelyi/lazy.lua
@@ -53,9 +53,10 @@ require("lazy").setup({
         dependencies = {
             "nvim-java/lua-async-await",
             "nvim-java/nvim-java-core",
+            "nvim-java/nvim-java-refactor",
             "nvim-java/nvim-java-test",
             "nvim-java/nvim-java-dap",
-            "unifTanjim/nui.nvim",
+            "MunifTanjim/nui.nvim",
             "neovim/nvim-lspconfig",
             "mfussenegger/nvim-dap",
             {
